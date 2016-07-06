9ac6128a2d4402aaef9cd78dcefaa5cd6cfaf34d
monicadivya <monicadivya9995@gmail.com>
2016-07-06 12:44:22 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,12 @@
+#include<studio.h>
+int main()
+{
+char c;
+printf("enter the character");
+scanf("%c",&c);
+if((c>='a'&&c<='z')||(c>='A'&&C<='Z'))
+printf("its a alphabet");
+else
+printf("its not a alphabet");
+return 0;
+}
